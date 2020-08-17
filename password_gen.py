4423edba9bebcf035f134f22017762f8195c62f3
Thais Leal <58890658+thaispll@users.noreply.github.com>
2020-08-17 16:15:17 -0300

    Add files via upload

---
+++ password_gen.py
@@ -0,0 +1,20 @@
+import random
+import PysSimpleGUI as sg
+import os
+
+class PassGen:
+        def _init_(self):
+            sg.theme('Black')
+            layout = [
+                [sg.Text('Site/Software', sinze=)]]
+
+            
+            pass
+
+        def Iniciar(self):
+            pass
+        def salvar_senha(self):
+            pass
+
+gen = PassGen()
+gen.Iniciar()
